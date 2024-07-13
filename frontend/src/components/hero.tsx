2b1952b1c4e6c23edff7c4631f7623e863d7c697
dixitpiyush <dixit.piyush3435@gmail.com>
2024-07-13 19:25:51 +0530

    Change hero and title texts

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,12 +1,12 @@
 function hero() {
   return (
     <div className="mb-8 flex max-w-3xl flex-1 flex-col justify-end text-slate-50">
-      <h1 className="cursor-text text-xl font-bold md:text-4xl">Journey Beyond Horizons</h1>
+      <h1 className="cursor-text text-xl font-bold md:text-4xl">Leaving Traces Behind</h1>
       <p className="my-4 cursor-text text-base md:text-lg">
-        Dive into the world of travel with stories that transport you to far-off lands. Adventure
-        awaits around every corner. It's time to explore the world!
+        Immerse yourself in the realm of travel through tales that whisk you away to distant places.
+        Excitement lies in every direction. It's your moment to discover the world!
       </p>
-      <div className="cursor-text text-sm font-semibold md:text-xl">Let's go!</div>
+      <div className="cursor-text text-base font-bold md:text-xl">Let's go!</div>
     </div>
   );
 }
